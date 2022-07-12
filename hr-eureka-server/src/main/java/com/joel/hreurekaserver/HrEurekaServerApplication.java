1e70f0fea50d911864789cb0cbbce5e1df3c2df4
Joel Maciel <soldado25461@gmail.com>
2022-07-12 13:04:23 -0300

    Configure Eureka clients

--- hr-eureka-server/src/main/java/com/joel/hreurekaserver/HrEurekaServerApplication.java
+++ hr-eureka-server/src/main/java/com/joel/hreurekaserver/HrEurekaServerApplication.java
@@ -1,6 +1,7 @@
 package com.joel.hreurekaserver;
 
 import org.springframework.boot.SpringApplication;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
