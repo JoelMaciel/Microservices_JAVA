c93333d1925aaca4db46b071c5bbed5ca6d56947
Joel Maciel <soldado25461@gmail.com>
2022-07-13 08:33:27 -0300

    Database seeding

--- hr-user/src/main/java/com/joel/hruser/entities/User.java
+++ hr-user/src/main/java/com/joel/hruser/entities/User.java
@@ -28,7 +28,7 @@ public class User implements Serializable {
 	private String password;
 
 	@ManyToMany(fetch = FetchType.EAGER)
-	@JoinTable(name = "tb_user_id", joinColumns = @JoinColumn(name = "user_id"),
+	@JoinTable(name = "tb_user_role", joinColumns = @JoinColumn(name = "user_id"),
 	inverseJoinColumns = @JoinColumn(name = "role_id"))
 	private Set<Role> roles = new HashSet<>();
 
